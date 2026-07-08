69641e9e90ca446cb6c66e47d85dd28bec013100
Karen Spriggs <k.spriggs01@gmail.com>
2026-07-08 18:47:12 -0500

    user controller with decorators

--- jsm-nestjs/src/user/user.controller.ts
+++ jsm-nestjs/src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // GET /user
+  @Get()
+  getUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: name || 'John Doe' },
+      { id: 2, name: 'Jane Doe' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    }
+
+    return users;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id, name: 'John Doe' };
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return {
+      data: createUserDto,
+      message: 'User Created successfully',
+    };
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: { id, ...updateUserDto },
+      message: 'User Updated successfully',
+    };
+  }
+}
